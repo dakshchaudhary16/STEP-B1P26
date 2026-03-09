e1588151a9dbf2cb130803c3976a82fd54682268
Daksh Chaudhary <dakshdagur953@gmail.com>
2026-03-09 22:05:17 +0530

    UC11: Object-oriented palindrome service

--- src/Hello/app.java
+++ src/Hello/app.java
@@ -2,26 +2,10 @@ public class app {
 
     public static void main(String[] args){
 
-        String word = "Level";
+        String word = "level";
 
-        word = word.toLowerCase();
+        PalindromeService service = new PalindromeService();
 
-        int left = 0;
-        int right = word.length() - 1;
-
-        boolean palindrome = true;
-
-        while(left < right){
-
-            if(word.charAt(left) != word.charAt(right)){
-                palindrome = false;
-                break;
-            }
-
-            left++;
-            right--;
-        }
-
-        System.out.println(palindrome ? "Palindrome" : "Not Palindrome");
+        System.out.println(service.checkPalindrome(word));
     }
 }
