cf919ecab26980f6df44d4c361dc3e3ae2b21ea6
Daksh Chaudhary <dakshdagur953@gmail.com>
2026-03-09 22:08:23 +0530

    UC13: Performance comparison

--- src/Hello/app.java
+++ src/Hello/app.java
@@ -1,11 +1,47 @@
 public class app {
 
+    static boolean reverseMethod(String word){
+
+        String reversed="";
+
+        for(int i=word.length()-1;i>=0;i--){
+            reversed += word.charAt(i);
+        }
+
+        return word.equals(reversed);
+    }
+
+    static boolean iterativeMethod(String word){
+
+        int left=0;
+        int right=word.length()-1;
+
+        while(left<right){
+
+            if(word.charAt(left)!=word.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args){
 
-        String word = "level";
+        String word="level";
 
-        PalindromeStrategy strategy = new StackStrategy();
+        long start = System.nanoTime();
+        reverseMethod(word);
+        long end = System.nanoTime();
 
-        System.out.println(strategy.check(word));
+        System.out.println("Reverse Method Time: "+(end-start));
+
+        start = System.nanoTime();
+        iterativeMethod(word);
+        end = System.nanoTime();
+
+        System.out.println("Iterative Method Time: "+(end-start));
     }
 }
